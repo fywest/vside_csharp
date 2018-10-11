181640e4c68eaf7ae12b6cd0de356ffb83357154
fywest <fywest2109@hotmail.com>
2018-10-11 12:46:01 +0300

    week1 task add

---
+++ WindowsFormsApp1/Week1_task/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Week1_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "mm:ss:msec";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(165, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 22);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 179);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "stopwatch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+
+        private int min; 
+        private int sec;
+        private int msec;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Week1_task/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week1_task
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+                    
+           msec++;
+            if(msec==100)
+            {
+                sec++;
+                msec = 0;
+                if(sec==60)
+                {
+                    min++;
+                    sec = 0;
+                    if(min==60)
+                    {
+                        min = 0;
+                    }
+
+                }
+            }
+
+            label1.Text = string.Format("{0:00}", min) + " : " + string.Format("{0:00}", sec) + " : " + string.Format("{0:00}", msec);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.min = 0;
+            this.sec = 0;
+            this.msec = 0;
+            label1.Text = string.Format("{0:00}", min) + " : " + string.Format("{0:00}", sec) + " : " + string.Format("{0:00}", msec);
+        }
+    }
+}
