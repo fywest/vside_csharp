1547a70bdfb62a65b3d96c7bda8ea9656a534dae
fywest <fywest2109@hotmail.com>
2018-10-16 13:28:14 +0300

    update demo

---
+++ WindowsFormsApp1/WindowsFormsApp1/Class_test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    public class Class_test
+    {
+        private int field_test;
+
+        public int Property_test
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+
+    internal delegate void Delegate_test();
+
+    public interface Interface_test
+    {
+    }
+
+    public class Class_test_sub : Class_test
+    {
+    }
+}
