741f34ae192f50f83f3bc893cd96cb5eda5b18ba
fywest <fywest2109@hotmail.com>
2018-11-15 15:07:33 +0200

    add static class

--- ConsoleApp1/CSharpClassStruct/Class1.cs
+++ ConsoleApp1/CSharpClassStruct/Class1.cs
@@ -114,6 +114,159 @@ namespace CSharpClassStruct
             this.originalItemID = originalID;
         }
     }
+
+    public class Shape
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Peforming base class drawing tasks");
+        }
+        public void set_X_Y(int x,int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    class Circle:Shape
+    {
+        public override void Draw()
+        {
+            
+            Console.WriteLine("Drawing a circle");
+            //base.Draw();
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - {2} - {3}", this.X, this.Y, this.Height,this.Width);
+        }
+    }
+    class Rectangle:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a rectangle");
+            //base.Draw();
+
+        }
+    }
+    class Triangle:Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a triangle");
+            //base.Draw();
+        }
+
+    }
+
+    namespace OverrideAndNew
+    {
+        class BaseClass
+        {
+            public virtual void Method1()
+            { Console.WriteLine("Base - Method1"); }
+
+            public virtual void Method2()
+            { Console.WriteLine("Base - Method2"); }
+        }
+
+        class DerivedClass:BaseClass
+        {
+            public override void Method1()
+            { Console.WriteLine("Derived - Method1"); }
+
+            public new void Method2()
+            { Console.WriteLine("Derived - Method2"); }
+        }
+
+        public class Program
+        {
+            public static void baseDerived()
+            {
+                BaseClass bc = new BaseClass();
+                DerivedClass dc = new DerivedClass();
+                BaseClass bcdc = new DerivedClass();
+
+                bc.Method1();
+                bc.Method2();
+
+                dc.Method1();
+                dc.Method2();
+
+                bcdc.Method1();
+                bcdc.Method2();
+
+                var tests = new List<BaseClass> { bc, dc, bcdc };
+                Console.WriteLine("----------");
+                foreach(var test in tests)
+                { test.Method1();test.Method2(); }
+                Console.WriteLine("----------");
+            }
+        }
+
+    }
+
+    namespace TemperatureConverter
+    {
+        public static class TemperatureConverter
+        {
+            public static double CelsiusToFahrenheit(string temperatureCelsius)
+            {
+                double celsius = Double.Parse(temperatureCelsius);
+                double fahrenheit = (celsius * 9 / 5) + 32;
+
+                return fahrenheit;
+            }
+
+
+            public static double FahrenheitToCelsius(string temperatureFahrenheit)
+            {
+                double fahrenheit = Double.Parse(temperatureFahrenheit);
+                double celsius = (fahrenheit - 32) * 5 / 9;
+
+                return celsius;
+            }
+        }
+
+        class TestTemperatureConverter
+        {
+            public static void test()
+            {
+                Console.WriteLine("Please select the convertor direction");
+                Console.WriteLine("1. From Celsius to Fahrenheit.");
+                Console.WriteLine("2. From Fahrenheit to Celsius.");
+                Console.Write(":");
+
+                string selection = Console.ReadLine();
+                double F, C = 0;
+
+                switch (selection)
+                {
+                    case "1":
+                        Console.Write("Please enter the Celsius temperature: ");
+                        F = TemperatureConverter.CelsiusToFahrenheit(Console.ReadLine());
+                        Console.WriteLine("Temperature in Fahrenheit: {0:F2}", F);
+                        break;
+
+                    case "2":
+                        Console.Write("Please enter the Fahrenheit temperature: ");
+                        C = TemperatureConverter.FahrenheitToCelsius(Console.ReadLine());
+                        Console.WriteLine("Temperature in Celsius: {0:F2}", C);
+                        break;
+
+                    default:
+                        Console.WriteLine("Please select a convertor.");
+                        break;
+                }
+
+            }
+        }
+    }
     class Program
     {
         static void customClass()
@@ -154,7 +307,7 @@ namespace CSharpClassStruct
 
             Console.WriteLine(change.ToString());
             //delete item instance
-            item = null;
+            //item = null;
             
             //trial
             WorkItem item1 = new WorkItem("trial Fix Bugs",
@@ -168,17 +321,48 @@ namespace CSharpClassStruct
 
             Console.WriteLine(item1.ToString());
             Console.WriteLine(change1.ToString());
+            if(item.Equals(item1))
+            {
+                Console.WriteLine("item is same as item1");
 
+            }
+            else
+            {
+                Console.WriteLine("item is difference with item1");
+            }
             
             
         }
+
+        static void shape()
+        {
+            var shapes = new List<Shape>
+            {
+                new Circle(),
+                new Rectangle(),
+                new Triangle()
+               
+            };
+            shapes[0].Width = 30;
+            shapes[0].Height = 40;
+            shapes[0].set_X_Y(10, 15);
+            Console.WriteLine(shapes[0]);
+            foreach(var shape in shapes)
+            {
+                shape.Draw();
+            }
+            
+        }
+
 
         static void Main(string[] args)
         {
             //customClass();//construct person class
             //testPerson();//construct custom class
-            workItem();//construct inheritance class
-
+            //workItem();//construct inheritance class
+            //shape();//Polymorphism usage
+            //OverrideAndNew.Program.baseDerived();//base and derived class
+            TemperatureConverter.TestTemperatureConverter.test();//static class
 
             Console.ReadKey();
         }
